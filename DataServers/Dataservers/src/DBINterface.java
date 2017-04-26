eed2d918aa801c29fb40d82ddf042cf829596933
Nikhilesh912 <kshirsagar.nikhilesh@gmail.com>
2017-04-26 15:35:29 -0400

    Added DB interface functions
    Added functions getOriginalTableData with range and writeOriginalData

--- DataServers/Dataservers/src/DBINterface.java
+++ DataServers/Dataservers/src/DBINterface.java
@@ -47,11 +47,53 @@ public class DBINterface {
     }
 
     JSONObject getOriginalTableData(int[] range){
-        return new JSONObject();
+        try{
+            Statement stmt = con.createStatement();
+            String query = "select * from " + dbs.primaryTable + " where hash where hash between "
+                    + range[0] + " and " + range[1] + ";";
+            ResultSet rs = stmt.executeQuery(query);
+            JSONArray resultArray = new JSONArray();
+
+            while(rs.next()){
+                JSONObject result = new JSONObject();
+                result.append("article_title", rs.getString(1));
+                result.append("article_content", rs.getString(2));
+                result.append("author", rs.getString(5));
+                result.append("image_name", rs.getString(6));
+                result.append("hash", rs.getString(7));
+                resultArray.put(result);
+            }
+        return new JSONObject().put("originalData", resultArray);
+    }
+    catch (SQLException e){
+        e.printStackTrace();
+    }
+        return null;
     }
 
     void writeOriginalData(JSONObject obj){
+        JSONArray dataJSON = (JSONArray) obj.get("originalData");
+        try {
+            String insertArticle = "INSERT INTO " + dbs.primaryTable +
+                    " (article_title, article_content, author, image_name, hash)"
+                    + " VALUES(?,?,?,?,?)";
 
+            PreparedStatement psArticles = con.prepareStatement(insertArticle);
+
+            for (int i = 0; i < dataJSON.length(); i++) {
+                JSONObject row = (JSONObject) dataJSON.get(i);
+                psArticles.setString(1, row.get("article_title").toString());
+                psArticles.setString(2, row.get("article_content").toString());
+                psArticles.setString(5, row.get("author").toString());
+                psArticles.setString(6, row.get("image_name").toString());
+                psArticles.setString(7, row.get("hash").toString());
+                psArticles.addBatch();
+            }
+            psArticles.executeBatch();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
